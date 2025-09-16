b8e36a1bffe72f310083f7c6e5fc466d02d6a653
IAM-DEV88 <jaguerx88@gmail.com>
2025-09-15 19:11:11 -0500

    new look

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  
+  // Desactivar la barra de desarrollo de Astro
+  devToolbar: {
+    enabled: false
+  }
 });
