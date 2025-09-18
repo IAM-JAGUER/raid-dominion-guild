f4a52a0d6d80c2a9dac8c278e35c0792b83be360
IAM-DEV88 <jaguerx88@gmail.com>
2025-09-18 14:08:47 -0500

    look

--- src/data/features.js
+++ src/data/features.js
@@ -1,5 +1,10 @@
 export const features = [
   {
+    title: "Codex de la Hermandad",
+    icon: "📜",
+    description: "Codex que contiene todas las mecánicas y reglas de las raids."
+  },
+  {
     title: "Gestión de Bandas",
     icon: "👥",
     description: "Control total sobre la formación y organización de tu banda de raid con solo unos clics."
@@ -15,14 +20,19 @@ export const features = [
     description: "Asegúrate de que todos los buffs necesarios estén cubiertos en tu grupo."
   },
   {
-    title: "Interfaz Personalizable",
-    icon: "🎨",
-    description: "Adapta la interfaz a tus preferencias y necesidades específicas."
+    title: "Integración con Discord",
+    icon: "🎯",
+    description: "Almacena y comparte el link de Discord para una experiencia más fluida."
   },
   {
-    title: "Alertas Inteligentes",
+    title: "Alertas con un click",
     icon: "🔔",
-    description: "Recibe notificaciones importantes durante las incursiones."
+    description: "Alerta a la banda con notificaciones importantes durante las incursiones."
+  },
+  {
+    title: "Interfaz Discreta",
+    icon: "🎨",
+    description: "Interfaz que se adapta en un espacio pequeño y discreto."
   },
   {
     title: "Soporte Técnico",
